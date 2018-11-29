84d848a603ff9377a4e7fa42179a69bd8e0a6cf7
Damián Maenza <daezma@users.noreply.github.com>
2018-11-29 11:49:29 -0300

    Hola!

--- src/components/AppFrame.js
+++ src/components/AppFrame.js
@@ -3,32 +3,37 @@ import PropTypes from 'prop-types';
 import MenuAppBar from './MenuAppBar';
 import { withRouter } from 'react-router-dom';
 import InitPage from './InitPage';
+import { observer, inject } from 'mobx-react';
 
-class AppFrame extends Component {
-  render() {
-    const { clase } = this.props;
-    const contenido = (() => {
-      switch (clase) {
-        case 'blank':
-          return <InitPage />;
-        default:
-          break;
-      }
-    })();
+const AppFrame = inject('login')(
+  observer(
+    class AppFrame extends Component {
+      render() {
+        const { clase } = this.props;
+        const contenido = (() => {
+          switch (clase) {
+            case 'blank':
+              return <InitPage />;
+            default:
+              break;
+          }
+        })();
 
-    return (
-      <div>
-        <div className="app-frame">
-          <MenuAppBar />
-          <div>{contenido}</div>
+        return (
           <div>
-            <p>Tgroup Sistemas</p>
+            <div className='app-frame'>
+              <MenuAppBar />
+              <div>{contenido}</div>
+              <div>
+                <p>Tgroup Sistemas</p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    );
-  }
-}
+        );
+      }
+    }
+  )
+);
 
 AppFrame.propTypes = {
   body: PropTypes.element
